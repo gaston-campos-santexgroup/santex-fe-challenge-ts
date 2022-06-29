b7d195589aa1292e9b0725ca7f87835a89077817
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-06-28 21:01:38 -0300

    product component created

---
+++ src/components/Product.tsx
@@ -0,0 +1,20 @@
+import { ItemCard } from '../interfaces/product.interface'
+
+export const Product: React.FC<ItemCard> = ({ item, buyItem }) => {
+
+    return (
+        <div className='product'>
+            <img src={item.assets[0].source} alt={item.name} />
+            <div className="name-slug">
+                <div className="name">{item.name}</div>
+                <div className="slug">{item.slug}</div>
+                <div className="price">${item.variantList.items[0].price}</div>
+            </div>
+            <div className="description">{item.description}</div>
+            <button onClick={(e) => {
+                e.preventDefault();
+                buyItem(item);
+            }} >Agregar al Carrito</button>
+        </div>
+    )
+}
