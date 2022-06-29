49de51640fd071f5a9a4e253caaa812e9a6f0f88
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-06-28 21:39:33 -0300

    product list or cart routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header'
 import './App.scss'
 import { Item } from './interfaces/product.interface';
 import { Product } from './components/Product';
 import productList from './data/products.json'
+import { Cart } from './components/Cart';
+
 
 function App() {
   const [totalPrice, addTotalPrice] = useState(0);
@@ -14,14 +17,21 @@ function App() {
     addTotalPrice(totalPrice + item.variantList.items[0].price)
   }
 
-
-
   return (
     <>
-      <Header totalPrice={totalPrice} totalQuantity={totalQuantity}></Header>
-      {productList.data.products.items.map((item: Item) => (
-        <Product item={item} buyItem={buyItem} />
-      ))}
+      <BrowserRouter>
+        <Header totalPrice={totalPrice} totalQuantity={totalQuantity}></Header>
+
+
+        <Routes>
+          <Route path="/" element={
+            productList.data.products.items.map((item: Item) => (
+              <Product item={item} buyItem={buyItem} />
+            ))
+          } />
+          <Route path="cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { Totals } from '../interfaces/totals.interface';
 
 export const Header: React.FC<Totals> = ({ totalPrice, totalQuantity }) => {
     return (
         <header className='header'>
-            <img className='logo' src="https://santex.wpengine.com/wp-content/uploads/2019/02/logo-santex@3x.png" alt="logo" />
-            <div className='totals'>
-                <div className='amount'>precio total: ${totalPrice.toLocaleString('es-AR', { minimumFractionDigits: 0 })}</div>
-                <div className='quantity'>cantidad: {totalQuantity} unidades</div>
-            </div>
+            <Link to="/">
+                <img className='logo' src="https://santex.wpengine.com/wp-content/uploads/2019/02/logo-santex@3x.png" alt="logo" />
+            </Link>
+            <Link to="/cart">
+                <div className='totals'>
+                    <div className='amount'>precio total: ${totalPrice.toLocaleString('es-AR', { minimumFractionDigits: 0 })}</div>
+                    <div className='quantity'>cantidad: {totalQuantity} unidades</div>
+                </div>
+            </Link>
         </header>
     )
 }
