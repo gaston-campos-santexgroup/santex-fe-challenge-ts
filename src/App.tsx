9b8967107700cc8cc52253be5c34a96ca41f51ee
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-06-28 23:09:51 -0300

    add cart product to array of cart and show it in cart component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header'
 import './App.scss'
-import { Item } from './interfaces/product.interface';
+import { CartProduct, Item } from './interfaces/product.interface';
 import { Product } from './components/Product';
 import productList from './data/products.json'
 import { Cart } from './components/Cart';
@@ -11,25 +11,39 @@ import { Cart } from './components/Cart';
 function App() {
   const [totalPrice, addTotalPrice] = useState(0);
   const [totalQuantity, addTotalQuantity] = useState(0);
+  const [cart, addItemToCart] = useState<CartProduct[]>([]);
 
   const buyItem = (item: Item) => {
     addTotalQuantity(totalQuantity + item.assets.length);
     addTotalPrice(totalPrice + item.variantList.items[0].price)
+    addItemToCart(prevStateCart => {
+      const cartItem: CartProduct = prevStateCart.find(x => x.id === item.id) ?? Object.assign({ quantity: 0 }, item);
+      const cartItemIndex: number = prevStateCart.findIndex(x => x.id === item.id);
+      cartItem.quantity = cartItem.quantity + 1;
+
+      if (cartItemIndex === -1) {
+        return [...prevStateCart, cartItem];
+      }
+      else {
+        prevStateCart[cartItemIndex] = cartItem;
+        return prevStateCart
+      }
+    });
   }
+
+
 
   return (
     <>
       <BrowserRouter>
         <Header totalPrice={totalPrice} totalQuantity={totalQuantity}></Header>
-
-
         <Routes>
           <Route path="/" element={
             productList.data.products.items.map((item: Item) => (
               <Product item={item} buyItem={buyItem} />
             ))
           } />
-          <Route path="cart" element={<Cart />} />
+          <Route path="cart" element={<Cart cart={cart} />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,8 +1,20 @@
-export const Cart = () => {
-  return (
-    <>
-    <div>Cart</div>
-    <p>items:</p>
-    </>
-  )
+import { CartProduct } from '../interfaces/product.interface'
+interface FuncProps {
+    cart: CartProduct[];
+}
+export const Cart: React.FC<FuncProps> = ({ cart }) => {
+    return (
+        <>
+            <div>Cart</div>
+            <p>items:</p>
+            {cart.map((product) => (
+                <div className="product-cart">
+                    <div className="name">{product.name}</div>
+                    <div className="price">${product.variantList.items[0].price.toLocaleString('es-AR', { minimumFractionDigits: 0 })}</div>
+                    <div className="quantity">{product.quantity}</div>
+                </div>
+            ))}
+
+        </>
+    )
 }
