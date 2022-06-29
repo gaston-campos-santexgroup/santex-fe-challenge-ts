0e74ec53f682a2481afa4e00500352fc33011663
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-06-28 21:02:42 -0300

    app with header and product list, buy button sums total amounts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Header } from './components/Header'
+import './App.scss'
+import { Item } from './interfaces/product.interface';
+import { Product } from './components/Product';
+import productList from './data/products.json'
 
 function App() {
+  const [totalPrice, addTotalPrice] = useState(0);
+  const [totalQuantity, addTotalQuantity] = useState(0);
+
+  const buyItem = (item: Item) => {
+    addTotalQuantity(totalQuantity + item.assets.length);
+    addTotalPrice(totalPrice + item.variantList.items[0].price)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header totalPrice={totalPrice} totalQuantity={totalQuantity}></Header>
+      {productList.data.products.items.map((item: Item) => (
+        <Product item={item} buyItem={buyItem} />
+      ))}
+    </>
   );
 }
 
-export default App;
+export default App
