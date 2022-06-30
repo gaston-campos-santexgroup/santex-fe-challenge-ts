9936b015c95f0715a4f5f1d1b529df12891ab44a
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-06-30 10:18:06 -0300

    db access configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloLink, createHttpLink, ApolloProvider } from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+const commerceLink = createHttpLink({
+  uri: 'https://demo.vendure.io/shop-api',
+  headers: {
+    authorization: localStorage.getItem('Auth-Token')
+      ? `Bearer ${localStorage.getItem('Auth-Token')}`
+      : '',
+  },
+});
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    const context = operation.getContext();
+    const authHeader = context.response.headers.get('Vendure-Auth-Token');
+
+    if (authHeader) {
+      localStorage.setItem('Auth-Token', authHeader);
+    }
+    return response;
+  });
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([afterwareLink, commerceLink]),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
