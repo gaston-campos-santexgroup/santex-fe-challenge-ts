481c355d879acee269f572a52061af73e6134ec1
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-07-01 13:53:36 -0300

    product detail component

---
+++ src/components/ProductDetail.tsx
@@ -0,0 +1,26 @@
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+import { PRODUCTBYID } from '../graphql/queries';
+import { Item } from '../interfaces/product.interface';
+
+export const ProductDetail = () => {
+    let params = useParams();
+    const { loading, error, data } = useQuery(PRODUCTBYID);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{`Error: ${error.message}`}</p>;
+
+    const productData: Item = data.product;
+    return (
+        <div className="product-detail">
+            <div className="groped-data-1">
+                <div className="name">{productData.name} ID: {params.productId}</div>
+                <div className="slug">{productData.slug}</div>
+                <div className="price">${productData.variantList.items[0].price.toLocaleString('es-AR', { minimumFractionDigits: 0 })}</div>
+            </div>
+            <div className="groped-data-2">
+                <img src={productData.assets[0].source} alt={productData.name} />
+                <div className="description">{productData.description}</div>
+            </div>
+        </div>
+    )
+}
