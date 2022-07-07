fd799461c975f4d9df8b6089b448cd692f892be4
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-07-07 12:28:39 -0300

    added cart data to mutation

--- src/graphql/mutations/mutations.ts
+++ src/graphql/mutations/mutations.ts
@@ -2,15 +2,27 @@ import { gql } from '@apollo/client';
 
 // Define mutation
 export const ADD_ITEM_TO_ORDER = gql`
-  mutation AddItemToOrder($productVariantId: ID!, $quantity: Int!) {
-    addItemToOrder(productVariantId: $productVariantId, quantity: $quantity) {
-      ... on Order{
-        totalQuantity
-        total
-        createdAt
-        id
-        code
-      }
+mutation AddItemToOrder($productVariantId: ID!, $quantity: Int!) {
+  addItemToOrder(productVariantId: $productVariantId, quantity: $quantity) {
+    ... on Order{
+      totalQuantity
+      total
+      createdAt
+      id
+      code
+      orderPlacedAt
+      state
+      lines
+      {
+        quantity
+        productVariant{
+          id            
+          productId
+          name
+          price
+        }
+      }        
     }
   }
+}
 `;
