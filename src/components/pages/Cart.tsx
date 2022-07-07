00496b90cfe04b7e3279afbc34842def59ae7015
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-07-07 12:29:45 -0300

    new cart interface implementation

--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -1,20 +1,20 @@
-import { CartProduct } from '../../interfaces'
+import { Line } from '../../interfaces'
 interface FuncProps {
-    cart: CartProduct[];
+    cart: Line[];
 }
 export const Cart: React.FC<FuncProps> = ({ cart }) => {
     return (
         <>
-            <div>Cart</div>    
+            <div>Cart</div>
             <div className="product-cart">
-                    <div className="name">Nombre Producto</div>
-                    <div className="price">Precio</div>
-                    <div className="quantity">Cantidad</div>
-                </div>        
+                <div className="name">Nombre Producto</div>
+                <div className="price">Precio</div>
+                <div className="quantity">Cantidad</div>
+            </div>
             {cart.map((product) => (
                 <div className="product-cart">
-                    <div className="name">{product.name}</div>
-                    <div className="price">${product.variantList.items[0].price.toLocaleString('es-AR', { minimumFractionDigits: 0 })}</div>
+                    <div className="name">{product.productVariant.name}</div>
+                    <div className="price">${product.productVariant.price.toLocaleString('es-AR', { minimumFractionDigits: 0 })}</div>
                     <div className="quantity">{product.quantity}</div>
                 </div>
             ))}
