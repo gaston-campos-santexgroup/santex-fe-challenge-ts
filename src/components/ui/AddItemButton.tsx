30234d8482c3dc4d957c04e629f62c928dc09a3d
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-07-08 11:52:32 -0300

    update order when adding a new roduct to cart using CartContext

--- src/components/ui/AddItemButton.tsx
+++ src/components/ui/AddItemButton.tsx
@@ -1,15 +1,18 @@
+import { useContext } from 'react';
 import { useMutation } from '@apollo/client';
+import { CartContext } from '../../context';
 import { ADD_ITEM_TO_ORDER } from '../../graphql';
 import { AddItemToOrderInputs } from '../../interfaces';
 
-export const AddItemButton: React.FC<AddItemToOrderInputs> = ({ itemVariantId, quantity, addItemToCart }) => {
+export const AddItemButton: React.FC<AddItemToOrderInputs> = ({ itemVariantId, quantity }) => {
 
     const [addItem, { data, loading, error }] = useMutation(ADD_ITEM_TO_ORDER, { variables: { productVariantId: itemVariantId, quantity: quantity } });
+    const { updateOrder } = useContext(CartContext);
 
     if (loading) return <button>Submitting...</button>;
     if (error) return <>`Submission error! ${error.message}`</>;
     if (data) {
-        addItemToCart(data.addItemToOrder);
+        updateOrder?.(data.addItemToOrder);
     }
 
     return (
