5b34320e85537c7373e1dd37dd5b5fe93908fd88
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-07-13 12:05:30 -0300

    add itemwith id #1  to cart response mocked

--- src/mock-data/index.js
+++ src/mock-data/index.js
@@ -1,3 +1,4 @@
 import mockedProductList from './products.json'
+import mockedAddedItemId1ToOrder from './mutation-add-item-response.json'
 
-export { mockedProductList }
+export { mockedProductList, mockedAddedItemId1ToOrder }
