bc2315085bd3316d7c8f29658f8fb6d4fcc7fd3f
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-07-15 12:22:10 -0300

    added roles to list and listItems

--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -6,21 +6,20 @@ export const Cart: React.FC = () => {
     const { order } = useContext(CartContext);
 
     return (
-        <>
-            <div>Cart</div>
+        <div role="list">
+            <div className="title">Cart</div>
             <div className="product-cart">
-                <div className="name">Nombre Producto</div>
-                <div className="price">Precio</div>
-                <div className="quantity">Cantidad</div>
+                <div className="name">Product Name</div>
+                <div className="price">Price</div>
+                <div className="quantity">Quantity</div>
             </div>
             {order.lines.map((product) => (
-                <div className="product-cart" key={`prod-${product.productVariant.id}`}>
+                <div className="product-cart" role="listitem" key={`prod-${product.productVariant.id}`}>
                     <div className="name">{product.productVariant.name}</div>
                     <div className="price">${product.productVariant.price.toLocaleString('es-AR', { minimumFractionDigits: 0 })}</div>
                     <div className="quantity">{product.quantity}</div>
                 </div>
             ))}
-
-        </>
+        </div>
     )
 }
